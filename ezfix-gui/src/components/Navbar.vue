a15cac7c8d23fdb147540c25c2b3034204193436
Gabriel Marques <gsm3@cin.ufpe.br>
2021-04-26 23:02:12 -0300

    Adding a navbar

---
+++ ezfix-gui/src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-card tile>
+        <v-toolbar 
+            short 
+            color="white" 
+        >
+            <v-icon size="100">$ezfix_logo</v-icon>
+
+            <v-spacer></v-spacer>
+
+            <v-btn text class="mx-2"><u>How it works?</u></v-btn>
+
+            <v-btn text class="mx-2"><u>Work with us</u></v-btn>
+
+            <v-btn color="primary black--text" rounded class="mx-2">Welcome back, Sérgio!</v-btn>
+        </v-toolbar>
+    </v-card>
+</template>
--- ezfix-gui/src/plugins/vuetify.js
+++ ezfix-gui/src/plugins/vuetify.js
@@ -2,11 +2,18 @@ import Vue from "vue";
 import Vuetify from "vuetify/lib/framework";
 import "@fortawesome/fontawesome-free/css/all.css";
 
+import EzfixLogo from '@/assets/icons/EzfixLogo.vue'
+
 Vue.use(Vuetify);
+
+const MY_ICONS = {
+  ezfix_logo:{component: EzfixLogo},
+}
 
 export default new Vuetify({
   icons: {
     iconfont: "fa",
+    values: MY_ICONS,
   },
   theme: {
     themes: {
