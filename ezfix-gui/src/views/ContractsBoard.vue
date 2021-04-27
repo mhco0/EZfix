e1d981060bd5933912f3c7904cc70ca95fa1abef
Thalisson <thalissonmt@gmail.com>
2021-04-27 20:19:47 -0300

    html started and fake data created on the Contracts Board page

---
+++ ezfix-gui/src/views/ContractsBoard.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div class="contracts-title">Contracts board view:</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ContractsBoard",
+  components: {},
+  data() {
+    return {
+      contracts: [],
+    };
+  },
+  created() {
+    this.contracts = [
+      {
+        id: 1,
+        serviceCategory: "House Cleaning",
+        paymentStatus: false,
+        paymentOnline: false,
+        name: "Flávio Cap",
+        avatar: "https://randomuser.me/api/portraits/men/3.jpg",
+      },
+      {
+        id: 2,
+        serviceCategory: "House Cleaning",
+        paymentStatus: true,
+        paymentOnline: true,
+        name: "Barnabé Cap",
+        avatar: "https://randomuser.me/api/portraits/men/4.jpg",
+      },
+      {
+        id: 3,
+        serviceCategory: "House Cleaning",
+        paymentStatus: true,
+        paymentOnline: true,
+        name: "Joana Cap",
+        avatar: "https://randomuser.me/api/portraits/women/2.jpg",
+      },
+    ];
+  },
+};
+</script>
+
+<style scoped>
+.contracts-title {
+  padding-top: 5%;
+  padding-left: 3%;
+  font-weight: 500;
+  font-size: 40px;
+}
+</style>
