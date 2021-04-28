b33bf3491bb87a52bfd816a68e04cd3979b8af18
Gabriel Marques <gsm3@cin.ufpe.br>
2021-04-28 01:05:48 -0300

    Listing coments at to contract page

--- ezfix-gui/src/views/ToContract.vue
+++ ezfix-gui/src/views/ToContract.vue
@@ -27,10 +27,23 @@
                 </v-col>
             </v-row>
         </v-card>
+        
+        <h1 class="mt-16">Reviews:</h1>
+
+        <v-card class="pa-5 rounded-xl ma-5" color="grey lighten-5" flat width="100%">
+            <ReviewBar
+                class="mx-5" 
+                v-for="(review, i) in reviews"
+                :key="i"
+                :client_name="review.client_name"
+                :coment="review.coment"
+            />
+        </v-card>
     </v-container>
 </template>
 
 <script>
+import ReviewBar from "../components/ReviewBar"
 
 export default {
     data: () => ({
@@ -38,7 +51,28 @@ export default {
         provider_name: "Flávio Playboy",
         provider_description: "Hi, as you already know my name is Flavio and I would love to help you! I have more than 5 years of experience in house cleaning. For me, nothing is more satisfiying then a good smelling bathroom. Fun fact, I am a architecture student and a use every money that I earn here to support my studies.",
         provider_average_evaluations: "4.8",
-        provider_jobs_number: 27
+        provider_jobs_number: 27,
+        reviews: [
+            {
+                client_name: "Thales",
+                coment: "Very top!!! Amazing work, thank you."
+            },
+            {
+                client_name: "Marcos",
+                coment: " Thank you so much, my bathrom does not smell like penis anymore."
+            },
+            {
+                client_name: "Marcos",
+                coment: "Thank you so much, my bathrom does not smell like penis anymore."
+            },
+            {
+                client_name: "Marcos",
+                coment: "Thank you so much, my bathrom does not smell like penis anymore."
+            },
+        ]
     }),
+    components: {
+        ReviewBar,
+    },
 }
 </script>
