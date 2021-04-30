38208612d36029fcb047faddf9edb1c5e5e2b43c
mhco0 <mhco@cin.ufpe.br>
2021-04-29 23:17:07 -0300

    Adding first version of AppointmentClock

---
+++ ezfix-gui/src/components/AppointmentClock.vue
@@ -0,0 +1,56 @@
+<template>
+    <v-menu
+    top
+    :offset-y="offset"
+    offset-x
+    >
+        <template v-slot:activator="{ on, attrs }">
+            <v-btn
+            icon
+            color="white"
+            dark
+            large
+            elevetion="4"
+            v-bind="attrs"
+            v-on="on"
+            >
+                <v-icon>far fa-clock</v-icon>
+            </v-btn>
+        </template>
+        <v-list>
+            <v-list-item
+            v-for="(item, index) in items"
+            :key="index"
+            >
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item>
+        </v-list>
+    </v-menu>
+</template>
+
+<script>
+    export default{
+        name : "AppointmentClock",
+        data : function(){
+            return {
+                items: [
+                        { title: 'Click Me' },
+                        { title: 'Click Me' },
+                        { title: 'Click Me' },
+                        { title: 'Click Me 2' },
+                    ],
+                offset: true,
+            }
+        },
+        props: {    
+        },
+        methods:{
+        }
+    }
+</script>
+
+<style scoped>
+    .v-btn {
+        background-color: rgba(33, 120, 183, 0.5);
+    }
+</style>
--- ezfix-gui/src/views/Chat.vue
+++ ezfix-gui/src/views/Chat.vue
@@ -4,7 +4,14 @@
             <TimeMessage v-for="(messageText, index) in messageList" :key="index" :text="messageText"/>
         </v-card>
         <v-card class="ChatBottomArea">
-            <InputTextChat/>
+            <v-row>
+                <v-col>
+                    <InputTextChat />
+                </v-col>
+                <v-col>
+                    <AppointmentClock />
+                </v-col>
+            </v-row>
         </v-card>
     </div>
 </template>
@@ -12,12 +19,14 @@
 <script>
     import TimeMessage from "../components/TimeMessage";
     import InputTextChat from "../components/InputTextChat";
+    import AppointmentClock from "../components/AppointmentClock";
 
     export default {
         name: "Chat",
         components: {
             TimeMessage,
-            InputTextChat
+            InputTextChat,
+            AppointmentClock,
         },
         data: function() {
             return {
