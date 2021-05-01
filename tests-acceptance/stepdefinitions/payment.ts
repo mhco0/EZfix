b992a339a2806e245b88eb801e7dd9249131c08a
Thalisson <thalissonmt@gmail.com>
2021-04-30 22:00:30 -0300

    refactoring e2e tests and scenarios

--- tests-acceptance/stepdefinitions/payment.ts
+++ tests-acceptance/stepdefinitions/payment.ts
@@ -8,55 +8,51 @@ var base_url = "http://localhost:3000/";
 var client_id = "1";
 
 defineSupportCode(function ({ Given, When, Then }) {
-    Given(/^I am on the "Payment" page$/, async () => {
+    Given(/^I am on the "([^\"]*)" page$/, async (title) => {
         await browser.get("http://localhost:8080/#/payment/1");
-        await expect(browser.getTitle()).to.eventually.equal('ezfix-gui');
+        await expect(element(by.id('payment-view-title')).getText()).to.eventually.equal(title.toString());
     })
 
     Given(/^I can see a form to add my credit card information$/, async () => {
         await element(by.id('credit-card-form'));
     })
 
-    When(/^I enter my "Credit Card" informations$/, async () => {
-        await element(by.id('input-card-name')).sendKeys("Sergio Soares");
-        await element(by.id('input-card-number')).sendKeys("1111222233334444");
-        await element(by.id('input-card-expiration-month')).sendKeys("11");
-        await element(by.id('input-card-expiration-year')).sendKeys("2025");
-        await element(by.id('input-card-cvv')).sendKeys("101");
-    })
+    When(/^I enter my cardholder name "([^\"]*)" card number "(\d*)" expiration month "(\d*)" expiration year "(\d*)" cvv "(\d*)"$/,
+        async (cardName, cardNum, expM, expY, cvv) => {
+            await element(by.id('input-card-name')).sendKeys(cardName.toString());
+            await element(by.id('input-card-number')).sendKeys(cardNum.toString());
+            await element(by.id('input-card-expiration-month')).sendKeys(expM.toString());
+            await element(by.id('input-card-expiration-year')).sendKeys(expY.toString());
+            await element(by.id('input-card-cvv')).sendKeys(cvv.toString());
+        })
 
     When(/^I confirm the payment$/, async () => {
         await element(by.id('confirm-payment-now')).click();
     })
 
-    Then(/^I go to the "Contracts Board" page$/, async () => {
-        await expect(element(by.id('contracts-board-title')).getText()).to.eventually.equal('Contracts board view:');
+    Then(/^I go to the "([^\"]*)" page$/, async (title) => {
+        await expect(element(by.id('contracts-board-title')).getText()).to.eventually.equal(title.toString());
     })
 
-    Then(/^I see that the "Payment Status" is paid$/, async () => {
-        await element(by.id('payment-status-paid'));
+    Then(/^I see that the payment status is "([^\"]*)"$/, async (status) => {
+        await element(by.id(`payment-status-${status}`));
     })
-
 
     Given(/^I can see a button to pay in person$/, async () => {
         await element(by.id('credit-card-form'));
     })
 
-    When(/^I choose the "Pay in person" option$/, async () => {
+    When(/^I choose the pay in person option$/, async () => {
         await element(by.id('pay-in-person-button')).click();
 
         var alert = await browser.switchTo().alert();
         await alert.accept();
     })
 
-    Then(/^I see that the "Payment Status" is pending$/, async () => {
-        await element(by.id('payment-status-pending'));
-    })
-
-    When(/^I enter the CVV "102" wrong$/, async () => {
+    When(/^I enter the CVV "(\d*)" wrong$/, async (cvv) => {
         var cvvInput = await element(by.id('input-card-cvv'));
         await cvvInput.clear().then(async function () {
-            await cvvInput.sendKeys('102');
+            await cvvInput.sendKeys(cvv.toString());
         })
     })
 
@@ -66,58 +62,64 @@ defineSupportCode(function ({ Given, When, Then }) {
         await alert.dismiss();
     })
 
-    When(/^I enter the card number "1111222233334449" wrong$/, async () => {
+    When(/^I enter the card number "(\d*)" wrong$/, async (cardNum) => {
         var cardNumberInput = await element(by.id('input-card-number'));
         await cardNumberInput.clear().then(async function () {
-            await cardNumberInput.sendKeys('1111222233334449');
+            await cardNumberInput.sendKeys(cardNum.toString());
         })
+    })
+
+    Given(/^I am at the "([^\"]*)" page$/, async (title) => {
+        await browser.get("http://localhost:8080/#/contracts");
+        await expect(element(by.id('contracts-board-title')).getText()).to.eventually.equal(title.toString());
     })
 
     When(/^I choose the "Pay Now" option$/, async () => {
         await element(by.id('pay-service-now')).click();
     })
 
-    Then(/^I can't see the "Payment Status" is pending$/, async () => {
-        const paymentsPending = await element.all(by.id('payment-status-pending'));
+    Then(/^I can't see the payment status is "([^\"]*)"$/, async (status) => {
+        const paymentsPending = await element.all(by.id(`payment-status-${status}`));
 
         await expect(paymentsPending.length).to.equal(0);
     })
 
-    Given(/^the system has two services registered with id "1" and id "2"$/, async () => {
+    Given(/^the system has two services registered with id "(\d*)" and id "(\d*)"$/, async (id1, id2) => {
         await request.get(base_url + "listcontracts/" + client_id)
             .then(body => {
-                expect(body.includes(`"id":1`)).to.equal(true);
-                expect(body.includes(`"id":2`)).to.equal(true);
+                expect(body.includes(`"id":${id1}`)).to.equal(true);
+                expect(body.includes(`"id":${id2}`)).to.equal(true);
             });
     })
 
-    Given(/^the system has no service with id "3"$/, async () => {
+    Given(/^the system has no service with id "(\d*)"$/, async (id) => {
         await request.get(base_url + "listcontracts/" + client_id)
             .then(body =>
-                expect(body.includes(`"id":3`)).to.equal(false)
+                expect(body.includes(`"id":${id}`)).to.equal(false)
             );
     })
 
-    When(/^I register a new service with the service provider id "2", payment status and online "true"$/, async () => {
-        var service = {
-            "json": {
-                "client_id": client_id,
-                "service_provider_id": 2,
-                "payment_status": true,
-                "payment_online": true
+    When(/^I register a new service with the service provider id "(\d*)", payment status "([^\"]*)" and online "([^\"]*)"$/,
+        async (provider_id, payment_status, payment_online) => {
+            var service = {
+                "json": {
+                    "client_id": client_id,
+                    "service_provider_id": provider_id,
+                    "payment_status": payment_status,
+                    "payment_online": payment_online
+                }
             }
-        }
 
-        await request.post(base_url + "service/" + "2", service)
-            .then(body =>
-                expect(body.success).to.equal("Successful service create")
-            );
-    })
+            await request.post(base_url + "service/" + provider_id, service)
+                .then(body =>
+                    expect(body.success).to.equal("Successful service create")
+                );
+        })
 
-    Then(/^the system has a service with id "3"$/, async () => {
+    Then(/^the system has a service with id "(\d*)"$/, async (id) => {
         await request.get(base_url + "listcontracts/" + client_id)
             .then(body =>
-                expect(body.includes(`"id":3`)).to.equal(true)
+                expect(body.includes(`"id":${id}`)).to.equal(true)
             );
     });
 
