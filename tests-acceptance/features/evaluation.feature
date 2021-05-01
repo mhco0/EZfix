dd0c306fdaa49219dc4ddc5f7025490dfb18e633
Gabriel Marques <gsm3@cin.ufpe.br>
2021-05-01 12:53:33 -0300

    Refactoring scenarios

--- tests-acceptance/features/evaluation.feature
+++ tests-acceptance/features/evaluation.feature
@@ -3,22 +3,22 @@ Feature: As a client
         And I want to see the previows and mine evalutations of him
 
 Scenario: Openning the evaluating dialog of a service provider
-Given I am at the Contracts Board page
-Given I can see a concluded service of the service provider with id "1"
-Given I can see the button Add a review of the service provider with id "1"
-When I click on the button Add a review of the service provider with id "1"
-Then I can see the evaluation dialog
+        Given I am at the Contracts Board page
+        Given I can see a service of the service provider "Flávio Cap"
+        Given I can see the button Add a review of the service provider "Flávio Cap"
+        When I click on the button Add a review of the service provider "Flávio Cap"
+        Then I can see the evaluation dialog
 
 Scenario: Evaluating the service provider
-Given I am at the evaluation dialog of the service provider with id "1"
-When I register the evaluation with Attendance "5", Punctuality "5", Service Quality "5", comment "Realy good service!" and Evaluate EZfiz "5"
-When I click on the Save button
-Then The evaluation dialog of the service provider with id "1" closes
+        Given I am at the evaluation dialog of the service provider "Flávio Cap"
+        When I register the evaluation with Attendance "5", Punctuality "2", Service Quality "5", comment "Really good service!" and Evaluate EZfiz "5"
+        When I click on the Save button
+        Then The evaluation dialog of the service provider closes
 
 Scenario: List the previows evaluations of a service provider
-Given I am at the To contract page of the service provider with id "1"
-Then I can see the evaluations list which has an evaluation with "Really good service!" on its content
+        Given I am at the To contract page of the service provider "Flávio Cap"
+        Then I can see the evaluations list which has an evaluation with "Really good service!" on its content
 
-Scenario: Trying to evaluating a service provider already evaluated
-When I try to evaluate the same service with Attendance "5", Punctuality "5", Service Quality "5", comment "Nice service!" and Evaluate EZfiz "5"
-Then The system doesn't store the evaluation
+Scenario: Trying to evaluating a service already evaluated
+        When I try to evaluate a service already evaluated
+        Then The system doesn't store the evaluation
--- tests-acceptance/stepdefinitions/evaluation.ts
+++ tests-acceptance/stepdefinitions/evaluation.ts
@@ -6,41 +6,96 @@ import request = require("request-promise");
 
 var base_url = "http://localhost:3000/";
 
+async function get_provider_element(name: any){
+    const providers_boards = await element.all(by.id('contract-border'));
+
+    var provider = [];
+    for(var i = 0; i < providers_boards.length; i++){
+        try{
+            const provider_name = await providers_boards[i].element(by.id("provider-name"));
+            await expect(provider_name.getText()).to.eventually.equal(name);
+            provider.push(providers_boards[i]);
+        }
+        catch(e){}
+    }
+
+    if(provider.length == 0){
+        throw "Didn't find a provider with name "+name;
+    }
+
+    return provider;
+}
+async function get_provider_add_review_button_element(name: any){
+    const provider_elements = await get_provider_element(name)
+
+    for(var i=0; i<provider_elements.length; i++){
+        try{
+            const button_element = await provider_elements[i].all(by.id('addreview-button'));
+            if(button_element.length > 0) return button_element[0];
+        }
+        catch(e){}
+    }
+
+    throw "There is no add review button for the provider "+name
+}
+
+async function get_evaluated_service() {
+    var service_id = -1
+    await request.get(base_url + "listcontracts/1").then(response => {
+        JSON.parse(response).forEach(contract => {
+            if(contract.has_evaluation == true){
+                service_id = contract.id;
+            }
+        });
+    })
+
+    if(service_id == -1) throw "There's no no evaluated service";
+
+    return service_id;
+}
+
 defineSupportCode(function ({ Given, When, Then }) {
     Given(/^I am at the Contracts Board page$/, async () => {
         await browser.get("http://localhost:8080/#/contracts");
         await expect(browser.getTitle()).to.eventually.equal('ezfix-gui');
     })
 
-    Given(/^I can see a concluded service of the service provider with id "1"$/, async () => {
-        const providers_names = await element.all(by.id('provider-name'));
-        await expect(providers_names[0].getText()).to.eventually.equal('Flávio Playboy');
+    Given(/^I can see a service of the service provider "([^\"]*)"$/, async (name) => {
+        await get_provider_element(name)
     })
 
-    Given(/^I can see the button Add a review of the service provider with id "1"$/, async () => {
-        await element(by.id('addreview-button'));
+    Given(/^I can see the button Add a review of the service provider "([^\"]*)"$/, async (name) => {
+        await get_provider_add_review_button_element(name)
     })
 
-    When(/^I click on the button Add a review of the service provider with id "1"$/, async () => {
-        await element(by.id('addreview-button')).click();
+    When(/^I click on the button Add a review of the service provider "([^\"]*)"$/, async (name) => {
+        const button = await get_provider_add_review_button_element(name);
+
+        button.click();
     })
 
     Then(/^I can see the evaluation dialog$/, async () => {
         await element(by.id('evaluationform-dialog'));
     })
 
-    Given(/^I am at the evaluation dialog of the service provider with id "1"$/, async () => {
-        await element(by.id('evaluationform-dialog'));
+    Given(/^I am at the evaluation dialog of the service provider "([^\"]*)"$/, async (name) => {
+        const provider_name = await element(by.id("dialog-provider-name"));
+        await expect(provider_name.getText()).to.eventually.contain(name)
     })
 
-    When(/^I register the evaluation with Attendance "5", Punctuality "5", Service Quality "5", comment "Realy good service!" and Evaluate EZfiz "5"$/, async () => {
-        const ratings = await element.all(by.css('[aria-label="Rating 5 of 5"]'));
+    When(/^I register the evaluation with Attendance "([^\"]*)", Punctuality "([^\"]*)", Service Quality "([^\"]*)", comment "([^\"]*)" and Evaluate EZfiz "([^\"]*)"$/, async (
+        attendance,
+        punctuality,
+        service_quality,
+        comment,
+        ezfix_evaluation
+    ) => {
+        await element(by.id("attendance")).element(by.css('[aria-label="Rating '+attendance+' of 5"]')).click();
+        await element(by.id("punctuality")).element(by.css('[aria-label="Rating '+punctuality+' of 5"]')).click();
+        await element(by.id("service-quality")).element(by.css('[aria-label="Rating '+service_quality+' of 5"]')).click();
+        await element(by.id("ezfix-evaluation")).element(by.css('[aria-label="Rating '+ezfix_evaluation+' of 5"]')).click();
 
-        for(var i = 0; i < ratings.length; i++){
-            ratings[i].click()
-        }
-
-        await element(by.id('coment-area')).sendKeys("Really good service!");
+        await element(by.id('coment-area')).sendKeys(String(comment));
     })
 
     When(/^I click on the Save button$/, async () => {
@@ -50,22 +105,50 @@ defineSupportCode(function ({ Given, When, Then }) {
         await alert.dismiss();
     })
 
-    Then(/^The evaluation dialog of the service provider with id "1" closes$/, async () => {
+    Then(/^The evaluation dialog of the service provider closes$/, async () => {
         const dialog = await element.all(by.id('evaluationform-dialog'));
 
         await expect(dialog.length).to.equal(0);
     })
 
-    Given(/^I am at the To contract page of the service provider with id "1"$/, async () => {
-        await browser.get("http://localhost:8080/#/tocontract/1");
+    Given(/^I am at the To contract page of the service provider "([^\"]*)"$/, async (name) => {
+        var id = -1;
+
+        await request.get(base_url + "listcontracts/1").then(response => {
+            JSON.parse(response).forEach(contract => {
+                if(contract.provider_name == String(name)){
+                    id = contract.provider_id;
+                }
+            });
+        })
+        if(id == -1) throw "Didn't find a provider with name "+name;
+
+        await browser.get("http://localhost:8080/#/tocontract/"+id.toString());
         await expect(browser.getTitle()).to.eventually.equal('ezfix-gui');
     })
-    Then(/^I can see the evaluations list which has an evaluation with "Really good service!" on its content$/, async () => {
+
+    Then(/^I can see the evaluations list which has an evaluation with "([^\"]*)" on its content$/, async (comment) => {
         const reviews = await element.all(by.id('reviewtext'))
-        await expect(reviews[0].getText()).to.eventually.equal('Sérgio: Really good service!');
+
+        for(var i = 0; i < reviews.length; i++){
+            try{
+                await expect(reviews[i].getText()).to.eventually.equal("Sérgio: " + String(comment));
+
+                return;
+            }
+            catch(e){}
+        }
+
+        throw "Didn't find the coment "+comment
     });
 
-    When(/^I try to evaluate the same service with Attendance "5", Punctuality "5", Service Quality "5", comment "Nice service!" and Evaluate EZfiz "5"$/, async () => {
+    When(/^I try to evaluate a service already evaluated$/, async () => {
+        await get_evaluated_service();
+    });
+
+    Then(/^The system doesn't store the evaluation$/, async () => {
+        const service_id = get_evaluated_service();
+
         const body:any = {
             "evaluator_id": 1,
             "attendance_rating": 5,
@@ -75,14 +158,8 @@ defineSupportCode(function ({ Given, When, Then }) {
             "coment": "Nice service!"
         }
 
-        await request.post(base_url + "evaluate/1", body).catch(response => {
+        await request.post(base_url + "evaluate/"+service_id.toString(), body).catch(response => {
             expect(response.error).to.equal('{"failure":"Error in evaluation"}');
-        })
-    })
-
-    Then(/^The system doesn't store the evaluation$/, async () => {
-        await request.get(base_url + "listcoments/1").then(response => {
-            expect(JSON.parse(response).coments[0].coment).not.equal("Nice service!")
         })
     })
 })
